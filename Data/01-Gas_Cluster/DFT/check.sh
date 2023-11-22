c76b8d9a8fe73e4a9900d34e774b003985e81697
benshi97 <shixubenjamin@gmail.com>
2023-11-22 19:38:32 +0000

    clear some data files

--- Data/01-Gas_Cluster/DFT/check.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-find . -type d | while read -r dir; do
-    if [[ ! -e "$dir/OUTCAR" ]]; then
-        echo "$dir does not contain OUTCAR"
-    fi
-done
-
--- Data/03-M4_MgO/DFT/Au4/grab.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-for i in 1 2 3 4
-do
-	cp ${i}/pbe/AD_SLAB/POSCAR POSCAR_${i}
-done
